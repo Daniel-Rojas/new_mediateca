dc639d9227c8d7d2606d230c4dd21431fd347aa8
Daniel Rojas <drdrojas584@tamu.edu>
2019-02-14 17:33:39 -0600

    updated sql driver class call

--- MediaTecaLoginServer/src/database/LoginDatabase.java
+++ MediaTecaLoginServer/src/database/LoginDatabase.java
@@ -10,7 +10,7 @@ public class LoginDatabase {
 
     public LoginDatabase() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException c) {
             System.out.println("Error: Could not find MySQL Driver");
             System.exit(0);
